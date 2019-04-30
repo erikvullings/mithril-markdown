81ebf297d796239f73b892496079825ed41ca1e4
Erik Vullings <erik.vullings@gmail.com>
2019-04-30 23:40:56 +0200

    Included codeblock

--- packages/example/src/components/inputs/editor-page.ts
+++ packages/example/src/components/inputs/editor-page.ts
@@ -33,40 +33,24 @@ Multiline actions should be performed on each line.
           // autoResize: false,
           markdown: state.markdown,
           onchange: (markdown) => {
-            // console.log(markdown);
+            console.log(markdown);
           },
         }),
 
         m(CodeBlock, {
-          code: `import { ContentEditable } from 'mithril-markdown';
+          code: `import { MarkdownEditor } from 'mithril-markdown';
 import m from 'mithril';
 
 ...
 
-    m(ContentEditable, {
-      // Original HTML input
-      html: state.html,
-      // Returns the updated HTML code
-      onchange: html => {
-        state.html = html;
-        console.log(html);
+    m(MarkdownEditor, {
+      preview: 200,
+      // style: 'height: 200px; max-height: 200px',
+      // autoResize: false,
+      markdown: state.markdown,
+      onchange: (markdown) => {
+        console.log(markdown);
       },
-      // Example to prevent the user from entering commas
-      onkeydown: e => {
-        if (e.key === ',') {
-          e.preventDefault();
-        }
-      },
-      // Replace the base tag, if needed
-      tagName: 'div',
-      // By default, &amp; etc are replaced by their normal counterpart when losing focus.
-      // cleanupHtml: false,
-      // By default, don't allow the user to enter newlines
-      // preventNewline: false,
-      // By default, select all text when the element receives focus
-      // selectAllOnFocus: false,
-      // By default, when pasting text, remove all HTML and keep the plain text.
-      // pasteAsPlainText: false,
     }),
 `,
         }),
