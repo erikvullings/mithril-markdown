3491db3c7a4327ec19cb8f5ef870561d3f4f34d9
Erik Vullings <erik.vullings@gmail.com>
2019-04-30 23:49:30 +0200

    Make HTML preview a little bigger.

--- packages/example/src/components/inputs/editor-page.ts
+++ packages/example/src/components/inputs/editor-page.ts
@@ -28,7 +28,7 @@ Multiline actions should be performed on each line.
         m('h2.header', 'MarkdownEditor'),
 
         m(MarkdownEditor, {
-          preview: 200,
+          preview: 400,
           // style: 'height: 200px; max-height: 200px',
           // autoResize: false,
           markdown: state.markdown,
